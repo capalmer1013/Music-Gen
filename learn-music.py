0f156948392c7c0fd61ed52c2b2a6485453dd3bf
Chris Palmer <capalmer1013@gmail.com>
2016-02-06 00:21:40 -0500

    working on stuff. trust me

--- learn-music.py
+++ learn-music.py
@@ -3,10 +3,18 @@ import filestream
 import musicAttributes
 from music21 import *
 
+titanium = "David_Guetta_Feat._Sia_-_Titanium_Piano.xml"
+bach = "Minuet-in-G-Minor.xml"
+
+# this will need uncommented in the future
+'''
 if len(sys.argv) == 2:
     filename = sys.argv[1]
 else:
     print "learn-music requires exactly 1 filename as an argument"
+'''
+
+filename = bach
 
 flatStream = filestream.getFlatStream(filename)
 
--- musicAttributes.py
+++ musicAttributes.py
@@ -1,8 +1,20 @@
 from music21 import *
+import sys
 
 
 def getTimeSignatures(flatStream):
+
+    dictOfTimeSigs = {}
     timeSignatures = flatStream.getElementsByClass(meter.TimeSignature)
+
     for i in timeSignatures:
-        print i
+        if i.offset in dictOfTimeSigs:
+            if dictOfTimeSigs[i.offset].ratioString != i.ratioString:
+                print "Time signature errors. Multiple time signatures in the same measure"
+                sys.exit()
+        else:
+            dictOfTimeSigs[i.offset] = i
+
+    return dictOfTimeSigs
     # return a dict indexed by each measure with time changes
+
