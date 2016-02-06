b1880bb7671ebdfc19beb6b0fac0277c07ce6d5d
Chris Palmer <capalmer1013@gmail.com>
2016-02-06 17:58:47 -0500

    new stuff

--- objects.py
+++ objects.py
@@ -1,3 +1,4 @@
+from music21 import *
 class rational:
     def __init__(self):
         self.numerator = 0
@@ -8,10 +9,29 @@ class rational:
 
 
 class myNote:
-    def __init__(self, inputNote):
-        self.note
+    def __init__(self, inputNote, flatStream ):
+        self.note = inputNote.name
+        listOfNotes = flatStream.getElementsByClass(note.Note)
+        notesAtSameOffset = [] # this will be for determining the tonality
+        listOfNoteOffsets = [] # this will be for finding the next note
+        offsetMapStream = flatStream.offsetMap
+
+        for element in listOfNotes:
+            listOfNoteOffsets.append(element.offset)
+        listOfNoteOffsets.sort()
+        offsetIndex = listOfNoteOffsets.index(inputNote.offset)
+
+        for element in listOfNotes:
+            if element.offset == inputNote.offset:
+                notesAtSameOffset.append(element)
+
+        for element in notesAtSameOffset:
+            # figure out the tonality here
+            
+
+
+
         self.nextNoteInterval
         self.currentTonality
         self.currentRhythmicDissonance
         self.thisNote = inputNote
-        
