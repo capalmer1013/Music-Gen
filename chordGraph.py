e76500fe1bcd9eb5a289fdb591be29007aedcc41
Chris Palmer <capalmer1013@gmail.com>
2016-02-06 17:18:23 -0500

    myNote class

--- chordGraph.py
+++ chordGraph.py
@@ -10,11 +10,12 @@ dominant = []
 submendiant = []
 leadingtone = []
 
+chordDegree = {}
 
 def createChordGraph(flatStream):
     chords = flatStream.getElementsByClass(chord.Chord)
     k = flatStream.analyze('key')
-    scaleNotes = k.chord.notes
+    scaleNotes = k.chord._notes
     # k.correlationCoefficient
     listOfChords = []
     for i in chords:
@@ -22,6 +23,7 @@ def createChordGraph(flatStream):
 
     for singleChord in listOfChords:
         listOfRankings = []
+        listOfRankingsFloat = []
         for chordFunction in toneChords:
             matchingNotes = objects.rational()
             for tempPitch in singleChord.pitchNames:
@@ -36,7 +38,21 @@ def createChordGraph(flatStream):
                     else:
                         matchingNotes.denominator += 1
             listOfRankings.append(matchingNotes)
+        for e in listOfRankings:
+            listOfRankingsFloat.append(e.getFloat())
+        if listOfRankingsFloat.index(max(listOfRankingsFloat)) not in chordDegree:
+            chordDegree[listOfRankingsFloat.index(max(listOfRankingsFloat))] = []
+            chordDegree[listOfRankingsFloat.index(max(listOfRankingsFloat))].append(singleChord)
+        else:
+            chordDegree[listOfRankingsFloat.index(max(listOfRankingsFloat))].append(singleChord)
 
+    adjacencyMatrix = [[0 for x in range(len(toneChords))] for x in range(len(toneChords))]
+    for i in range(len(adjacencyMatrix)):
+        for j in range(len(adjacencyMatrix)):
+            adjacencyMatrix[i][j] = objects.rational()
+
+
+    print "BALLS"
 
 
 
