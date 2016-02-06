099659fd67ed7020be65d4893c9ebb88887d3b12
Chris Palmer <capalmer1013@gmail.com>
2016-02-06 17:18:34 -0500

    forgot this

--- objects.py
+++ objects.py
@@ -2,3 +2,16 @@ class rational:
     def __init__(self):
         self.numerator = 0
         self.denominator = -1
+
+    def getFloat(self):
+        return float(self.numerator)/float(self.denominator)
+
+
+class myNote:
+    def __init__(self, inputNote):
+        self.note
+        self.nextNoteInterval
+        self.currentTonality
+        self.currentRhythmicDissonance
+        self.thisNote = inputNote
+        
