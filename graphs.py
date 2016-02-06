7b64a833b87bca2a2d19d42869e3ed9171ff55a2
Chris Palmer <capalmer1013@gmail.com>
2016-02-06 01:18:24 -0500

    going to bed

--- graphs.py
+++ graphs.py
@@ -1,10 +1,21 @@
 from music21 import *
 
-def createRhythmGraph(flatStream):
+
+# def createRhythmGraph(flatStream):
+#
+#
+# def noteRhythmicDissonance(note):
 
 
-def noteRhythmicDissonance(note):
+def measureRhythmicDissonance(flatStream, timeSigs):
+    listOfNotes = []
+    dictOfNotes = {}
 
+    for element in flatStream:
+        if isinstance(element, note.Note) or isinstance(element, chord.Chord):
+            listOfNotes.append(element)
 
-def measureRhythmicDissonance(flatStream):
+    for element in listOfNotes:
+        dictOfNotes[element.offset] = element
+
     # return list of rhythmic dissonances for measures
