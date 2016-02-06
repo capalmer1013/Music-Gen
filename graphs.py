9951036add0acdd2f12a482f747d33611710b879
Chris Palmer <capalmer1013@gmail.com>
2016-02-06 09:51:13 -0500

    I have no words

--- graphs.py
+++ graphs.py
@@ -6,19 +6,15 @@ import musicAttributes
 
 def createRhythmGraph(flatStream):
     dictOfTime = musicAttributes.getTimeSignatures(flatStream)
-    listOfRhythmicDissonances = measureRhythmicDissonance(flatStream, dictOfTime)
+    dictOfRhythmicDissonances = measureRhythmicDissonance(flatStream, dictOfTime)
 
-
-# def createRhythmGraph(flatStream):
-#
-#
-# def noteRhythmicDissonance(note):
 
 
 def measureRhythmicDissonance(flatStream, timeSigs):
     listOfNotes = []
     listOfOffsets = []
     dictOfNotes = {}
+    dictOfDissonances = {}
 
     for element in flatStream:
         if isinstance(element, note.Note) or isinstance(element, chord.Chord):
@@ -43,14 +39,47 @@ def measureRhythmicDissonance(flatStream, timeSigs):
         else:
             currentMeasure = element.measureNumber
             # figure out the rhythmic dissonance for the previous measure
+            runningCount = 0
             for i in tempMeasureList:
-                noteRhythmicDissonance(i, timeSigs)
+                runningCount += noteRhythmicDissonance(i, timeSigs)
+
+            if len(tempMeasureList) > 0:
+                measureDissonance = runningCount/len(tempMeasureList)
+            else:
+                # maybe it should be 1 I dont know
+                measureDissonance = 0
+            tempMeasureList[:] = []
+            tempMeasureList.append(element)
+            dictOfDissonances[currentMeasure] = measureDissonance
             # get the next measure started
 
-    heresAlist = []
-    return heresAlist
+    return dictOfDissonances
     # return list of rhythmic dissonances for measures
 
 
 def noteRhythmicDissonance(noteElement, timeSigs):
+    timeOffsets = []
+    for i in timeSigs:
+        timeOffsets.append(i)
 
+    timeOffsets.sort(reverse=True)
+
+    for i in timeOffsets:
+        # grab the highest time signature that is at or below the
+        if timeSigs[i].offset <= noteElement.offset:
+            currentTimeSignature = timeSigs[i]
+            break
+
+    # TODO handle other denominators like 8
+    # possibly not now that I'm gonna try to use beat strengths
+
+    # if currentTimeSignature.numerator % 2 == 0:
+    #     # even time signature
+    #
+    # else:
+    #     # probably an odd time signature
+
+    # really it returns beat consonance
+    # 1 - 0 ! being most consonant 0 being most dissonant.
+    # really this may need rewritten but who knows
+    return noteElement.beatStrength
